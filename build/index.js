6428bfff0081ae4c1bc8c5b8f6c42e27c68aabf0
Connor Imrie <connorimrie@outlook.com>
2017-04-08 11:32:44 +0100

    Bump version and try to fix imports

--- build/index.js
+++ build/index.js
@@ -5,10 +5,10 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.MongoDBServer = undefined;
 
-var _MongoDBServer2 = require('./MongoDBServer');
+var _MongoDBServer = require('./MongoDBServer');
 
-var _MongoDBServer3 = _interopRequireDefault(_MongoDBServer2);
+var _MongoDBServer2 = _interopRequireDefault(_MongoDBServer);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-exports.MongoDBServer = _MongoDBServer3.default;
+exports.MongoDBServer = _MongoDBServer2.default;
